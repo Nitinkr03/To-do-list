f942ce8954278e0dcb311df416d0fe20dac4a52f
Nitinkr03 <nitinkr03112006@gmail.com>
2025-01-29 01:37:27 +0530

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,39 @@
+let inputbox=document.querySelector(".input")
+let listcontainer=document.querySelector("#listcontainer")
+function addtask(){
+    if(inputbox.value===''){
+        alert("you must write something");
+    }
+    else{
+        let li=document.createElement("li");
+        li.innerHTML=inputbox.value;
+        listcontainer.appendChild(li);
+        let span=document.createElement("span");
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
+        savedata();
+    }
+    inputbox.value='';
+}
+listcontainer.addEventListener("click",function(e){
+    if(e.target.tagName==="LI"){
+        e.target.classList.toggle("checked");
+        savedata();
+
+    }
+    else if(e.target.tagName==="SPAN"){
+        e.target.parentElement.remove();
+        savedata();
+          
+    }
+},false);
+function savedata(){
+    localStorage.setItem("data",listcontainer.innerHTML);
+}
+function showtask(){
+    listcontainer.innerHTML=localStorage.getItem("data");
+}
+showtask();
+
+
+
